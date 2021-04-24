d9652f4c87c1aacc04b1b73eb1fcd79bfdb03e42
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-04-24 12:28:58 +0200

    Crear el BottomTabNavigator

--- src/navigator/MenuLateral.tsx
+++ src/navigator/MenuLateral.tsx
@@ -13,12 +13,12 @@ import {
   View,
 } from 'react-native';
 
-import {StackNavigator} from './StackNavigator';
 import {SettingsScreen} from '../screens/SettingsScreen';
 import {styles} from '../theme/appTheme';
+import {Tabs} from './Tabs';
 
 export type RootStackParams = {
-  StackNavigator: undefined;
+  Tabs: undefined;
   SettingsScreen: undefined;
 };
 
@@ -31,7 +31,7 @@ export const MenuLateral = () => {
     <Drawer.Navigator
       drawerType={width > height ? 'permanent' : 'front'}
       drawerContent={props => <MenuInterno {...props} />}>
-      <Drawer.Screen name="StackNavigator" component={StackNavigator} />
+      <Drawer.Screen name="Tabs" component={Tabs} />
       <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
     </Drawer.Navigator>
   );
@@ -57,7 +57,7 @@ const MenuInterno = ({
       <View style={styles.menuContainer}>
         <TouchableOpacity
           style={styles.menuBoton}
-          onPress={() => navigation.navigate('StackNavigator')}>
+          onPress={() => navigation.navigate('Tabs')}>
           <Text style={styles.menuTexto}>Navegación</Text>
         </TouchableOpacity>
 
