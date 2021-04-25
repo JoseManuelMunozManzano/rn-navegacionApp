bec03f894e608ca0076aac3fbfc5b1b1c4c1035f
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-04-25 07:26:45 +0200

    Iconos en nuestra aplicación - Instalaciones en Android

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,5 +1,8 @@
 import React, {useEffect} from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {Text, View} from 'react-native';
+
+import {styles} from '../theme/appTheme';
 
 export const Tab1Screen = () => {
   useEffect(() => {
@@ -7,8 +10,11 @@ export const Tab1Screen = () => {
   }, []);
 
   return (
-    <View>
-      <Text>Tab1Screen</Text>
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>Iconos</Text>
+      <Text>
+        <Icon name="airplane-outline" size={50} color="#900" />
+      </Text>
     </View>
   );
 };
